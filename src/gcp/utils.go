453a4d3827c931d863af496d04c52e6a8a5b6891
Hiroki Yorimitsu <yorimitsu@supinf.co.jp>
2022-01-03 04:28:04 +0900

    fix: method of judging whether or not it is running on gcp

--- src/gcp/utils.go
+++ src/gcp/utils.go
@@ -4,7 +4,5 @@ import "os"
 
 // RunningOnGCP returns true if the application is running on GCP.
 func RunningOnGCP() bool {
-	// this environment variable is automatically set
-	// when you run application on GCP
-	return os.Getenv("GOOGLE_CLOUD_PROJECT") != ""
+	return os.Getenv("RUNNING_ON_GCP") != ""
 }
